cc7bd9bede7ec831fe949e72c35ce06fc54a21b7
Snzy027 <sneezyjunior1@gmail.com>
2022-02-02 11:34:20 +0700

    tt

---
+++ config.sample.js
@@ -0,0 +1,25 @@
+module.exports = {
+    author: "Snzy#6172",
+    botName: "MDevHostManager",
+    maintenance: true,
+    owners: ["652074628298047512"],
+    permissions: "8",
+    mongo: "",
+    token: "TOKEN-HERE",
+    timeFormat: "HH:mm:ss",
+    prefix: "m!",
+    links: {
+        discord: "https://discord.gg/gN48Ptvuvb",
+        website: "https://panel.host.monitors.cf",
+    },
+    embed: {
+        color: "BLUE",
+        footer: "MDevHostManager"
+    },
+    deleteCommands: false,
+    disabledCommands: [],
+    log: {
+        commands: true,
+        errors: true,
+    },
+}
